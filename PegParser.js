5c3af7e4b23d04d1bd71404271ae8412f0b2e9b8
Oleg Bulavitchi <bulavitchi@gmail.com>
2012-09-03 19:17:03 +0100

    worked on parser implementation

--- PegParser.js
+++ PegParser.js
@@ -2,16 +2,44 @@ var parser = require('./parser.js'),
     fs = require('fs')
     ;
 
+var filesToProcess = ["./Visa Rules.txt", "./Mastercard Rules.txt"];
 
-fs.readFile('./rules.txt', function(err, data) {
-   if(err) {
-       console.log('error reading the data');
-   } else {
-       process(new String(data));
-   }
+fs.readFile('./rules.txt', function (err, data) {
+    if (err) {
+        console.log('error reading the data');
+    } else {
+        process(new String(data));
+    }
 });
 
+function getDescription(rule) {
+    var name = rule.categoryCode.description;
+    if (rule.scheme) {
+        name += " " + rule.scheme.description;
+    }
+    return name;
+}
+
+var aggregated = {};
+function aggregate () {
+
+}
+
 function process(toParse) {
-    var x = parser.parse(toParse);
-    //console.log(x);
+    var data = parser.parse(toParse);
+    console.log("Extracted rules " + data.length);
+    var len = data.length;
+    for (var i = 0; i < len; i++) {
+
+        var rule = data[i],
+            description = i + ". " + getDescription(rule);
+        if (rule.description && rule.description.trim() === "CONS PREMIUM STANDARD (INTRA SAMEA)") {
+            if (rule.qualification) {
+                description += "->" + rule.qualification.length;
+            }
+            console.log(description);
+            console.log(JSON.stringify(rule));
+        }
+
+    }
 }
--- processor.js
+++ processor.js
@@ -1,39 +1,184 @@
+var util = require('util');
+
 var VisaFields = {
 
+};
+
+
+MemberMap = {
+    A: "Acquirer",
+    I: "Issuer",
+    M: "Merchant"
+};
+RegionMap = {
+  MC: {
+      1: "United States",
+      A: "Canada",
+      B: "Latin America and the Caribbean",
+      C: "Asia/Pacific",
+      D: "Europe",
+      E: "South Asia/Middle East/Africa"
+  },
+  VI : {
+      1: "United States",
+      2: "Canada",
+      3: "EU",
+      4: "Asia Pacific",
+      5: "Latin America",
+      6: "CEMEA"
+  }
+};
+
+var CommonFields = {
+    AC: "Authorisation Characteristics",
+    AI: "Airline",
+    AB: "Acquirer BIN",
+    AR: "Authorisation Response",
+    AU: "Authorized",
+    AV: "Auth Verification Value",
+    BS: "Business Service Agreement",
+    BT: "Business Service Agreement Type",
+    CA: "CAT code (Customer Activated Terminal Type)",
+    CC: "Country Code of the %s are %s equal",
+    CP: "Method of Capture",
+    CQ: "Chip Qualified Issuer",
+    CR: "Car Rental",
+    CS: "Cardscheme",
+    CT: "Country Code",
+    CV: "Cardholder Validation",
+    DA: "Data Attribute", //this is a complex call that depends on the file structure
+    DB: "Refund Transaction",
+    DD: "processed in %s days %s: ",
+    EH: "Electronic Hot Card File",
+    FL: "Floor Limit",
+    HT: "Hotel",
+    DC: "Debit Card Indicator",
+    MO: "Mail Order",
+    MC: "MCC Ranges",
+    PR: "Product Code",
+    RC: "Region Code Comparison",
+    RE: "Region Code of the %s is %s one of ",
+    RP: "Requested Payment Service",
+    RT: "Requiring Transactions",
+    TK: "transaction amount converted to %s is %s less than %s"
+};
+var TemplateGenerators = {
+  DD: function(template, structure) {
+      return util.format(template, structure.days, structure.op, structure.data );
+  },
+  TK: function(template, structure) {
+      return util.format(template,  structure.currency,structure.op, structure.amount);
+  },
+  RE: function(template, structure) {
+      return util.format(template, MemberMap[structure.member], structure.op, structure.data);
+  },
+  CC: function (template, structure) {
+      var members = []
+          memberCount = structure.data.length;
+      for(var i = 0; i<memberCount; i++) {
+          members.push(MemberMap[structure.data[i]]);
+      }
+      return util.format(template,members, structure.op);
+    }
+};
+
+MasterCardFields = {
+
+};
+
+function getField(fieldCode) {
+    var result = CommonFields[fieldCode];
+    if(!result) {
+        result = VisaFields[fieldCode];
+    }
+    if(!result) {
+        result = MasterCardFields[fieldCode];
+    }
+    return result;
 }
+
 
 //{FIELD_NAME} is one of [_, _, _]
 //{FIELD_NAME} is not one of [_, _, _]
-function handleCollection(fieldMap, structure) {
+function handleCollection(structure) {
     var fieldCode = structure.code,
         operator = " one of",
         prefix =  (structure.op === 'Y')?" is":" is not";
 
     return {
-        sentence: fieldMap[fieldCode] + prefix + operator,
+        fieldCode: fieldCode,
+        sentence: getField(fieldCode) + prefix + operator,
         data: structure.data
     }
+}
+// is {FIELD_NAME}
+function handleBooleanCheck(structure) {
+    var operator = (structure.op === 'Y')?"is": "is not";
+    return {
+        fieldCode: structure.code,
+        sentence: getField(structure.code) + " " + operator
+    };
 }
 //{FIELD_NAME} is _
 //{FIELD_NAME} is not _
 function propertyCheck(fieldMap, structure) {
     var operator = (structure.op === 'Y')?" is ": " is not ";
     return {
-        sentence: fieldMap[structure.code] + operator,
+        sentence: getField(structure.code) + operator,
         data: structure.data
     };
 }
 
-function functionCall(fieldMap, structure) {
+function handleFunctionCall(structure) {
+    var sentenceTmpl = getField(structure.code),
+        fnGenerator = TemplateGenerators[structure.code],
+        sentence;
+    if(fnGenerator) {
+        sentence = fnGenerator(sentenceTmpl, structure);
+    }
     return {
-
+        code:structure.code,
+        sentence: sentence
     };
 }
 
+function handleRangeCheck(structure) {
+    var operator = (structure.op === 'Y')? "is": "is not " + "in the ranges",
+        ranges = [];
+        var availableCount = structure.data.length;
+   // for(var i = 0; availableCount - 1; i+=2) {
+    //    ranges.push({min: structure.data[i], max: structure.data[i+1]});
+    //}
+    return {
+        sentence: operator,
+        fieldCode: structure.code,
+        data: ranges
+    };
+}
+function handleCondition(structure) {
+    return {
+        sentence: getField(structure.code) + " " + structure.op + " " + structure.value,
+        fieldCode: structure.code
+    };
+}
+
+function presentAndNotEmpty (value) {
+    var result = (value !== undefined)?value.trim():undefined;
+    if(result !== undefined && result.length > 0) {
+        return result;
+    }
+}
+
+
 module.exports = {
     visa: VisaFields,
+    regions: RegionMap,
+    handleRange: handleRangeCheck,
+    handleBoolean: handleBooleanCheck,
     handleCollection: handleCollection,
     handleProperty: propertyCheck,
-    handleFunction: functionCall
+    handleFunction: handleFunctionCall,
+    valuePresent: presentAndNotEmpty,
+    handleCondition: handleCondition
 }
 
