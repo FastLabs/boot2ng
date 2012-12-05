2c0795e278d5fcfbb7b201b673bf3e460a7e2c1e
obulavitchi <oleg.bulavitchi@endava.com>
2012-12-05 12:50:50 +0000

    included the missing files

---
+++ suiteBuilder.js
@@ -0,0 +1,323 @@
+var interchange = require("./interchange.js"),
+    cardScheme = "Visa",
+    scheme = "Purchase",
+    catCode = "Inter Regional",
+    merchantCountry = undefined;
+
+var templates = {
+    Visa: {
+        Cash: {
+            data: {
+                scheme: "Cash"
+            },
+            "UK Domestic" : {
+                issuer: {
+                    country: "UK",
+                    region: "3"
+                },
+                merchant: {
+                    country: "UK",
+                    region: "3"
+                },
+                acquirer: {
+                    country: "UK",
+                    region: "3"
+                }
+            },
+            "Inter Regional" : {
+                "issuer":{
+                    "country":"IND",
+                    "region":"2"
+                },
+                "merchant":{
+                    "country":"AUS",
+                    "region":"4"
+                },
+                "acquirer":{
+                    "country":"DE",
+                    "region":"3"
+                }
+            },
+            "Intra Regional" : {
+                issuer: {
+                    country: "UK",
+                    region: "3"
+                },
+                merchant: {
+                    country: "UK",
+                    region: "3"
+                },
+                acquirer: {
+                    country: "DE",
+                    region: "3"
+                }
+            }
+
+        },
+        Funding: {
+            data: {
+                scheme: "Funding"
+            },
+            "UK Domestic" : {
+                issuer: {
+                    country: "UK",
+                    region: "3"
+                },
+                merchant: {
+                    country: "UK",
+                    region: "3"
+                },
+                acquirer: {
+                    country: "UK",
+                    region: "3"
+                }
+            },
+            "Inter Regional" : {
+                "issuer":{
+                    "country":"IND",
+                    "region":"2"
+                },
+                "merchant":{
+                    "country":"AUS",
+                    "region":"4"
+                },
+                "acquirer":{
+                    "country":"DE",
+                    "region":"3"
+                }
+            },
+            "Intra Regional" : {
+                issuer: {
+                    country: "UK",
+                    region: "3"
+                },
+                merchant: {
+                    country: "UK",
+                    region: "3"
+                },
+                acquirer: {
+                    country: "DE",
+                    region: "3"
+                }
+            }
+        },
+        Purchase: {
+            data: {
+                scheme: "Purchase"
+            },
+            "UK Domestic" : {
+                issuer: {
+                    country: "UK",
+                    region: "3"
+                },
+                merchant: {
+                    country: "UK",
+                    region: "3"
+                },
+                acquirer: {
+                    country: "UK",
+                    region: "3"
+                }
+            },
+            "Non UK Domestic" : {
+                issuer: {
+                    country: "UK",
+                    region: "3"
+                },
+                merchant: {
+                    country: "UK",
+                    region: "3"
+                },
+                acquirer: {
+                    country: "UK",
+                    region: "3"
+                }
+            },
+            "Exported Domestic" : {
+                issuer: {
+                    country: "UK",
+                    bin: "123",
+                    region: "3"
+                },
+                merchant: {
+                    country: "UK",
+                    region: "3"
+                },
+                acquirer: {
+                    country: "UK",
+                    region: "3"
+                }
+            },
+            "Intra Regional" : {
+                issuer: {
+                    country: "UK",
+                    region: "3",
+                    subRegion:"J"
+                },
+                merchant: {
+                    country: "UK",
+                    region: "3",
+                    subRegion: "b"
+                },
+                acquirer: {
+                    country: "DE",
+                    region: "3",
+                    subRegion:"a"
+                }
+            }
+        }
+    }
+}
+
+var mccCombinations = [];
+
+function mccRangeToString(mccRange) {
+    if(mccRange.low === mccRange.top) {
+        return mccRange.low;
+    }
+    return mccRange.low +"->"+ mccRange.top;
+}
+
+function collectMcc(mccCollection) {
+    var mccUID = "";
+    for(var i in mccCollection) {
+        mccUID = mccUID + mccRangeToString(mccCollection[i]) + ((i< mccCollection.length-1)?" or ":"");
+    }
+    if(mccCombinations.indexOf(mccUID) === -1) {
+        mccCombinations.push(mccUID);
+    }
+}
+function fromTemplates(templateList){
+    var result = {};
+    templateList.forEach(function(template) {
+        for(var field in template) {
+            if(typeof template[field] === "string") {
+                result[field] = template[field];
+            } else {
+                result[field] = fromTemplates([template[field]]);
+            }
+        }
+    });
+    return result;
+}
+
+function copyAttributes(source, destination) {
+   for(var i  in source) {
+       destination[i] = source[i];
+   }
+}
+
+function putIn(source, destination) {
+   var group = source.destination,
+       instance = destination;
+    if(group) {
+       instance = destination[group] || {};
+       destination[group] = instance;
+    }
+    instance[source.fieldName] = source.value;
+}
+
+interchange.loadRules(cardScheme, function(aggregated) {
+    var rules = aggregated[scheme][catCode].collection,
+        structure = aggregated[scheme][catCode].structure,
+        schemeTemplate = templates[cardScheme][scheme],
+        catCodeTemplate = schemeTemplate[catCode];
+    rules.forEach(function(rule) {
+        var result = fromTemplates([schemeTemplate.data, catCodeTemplate]);
+        if(structure.isFieldVisible("reimbursementAttribute")) {
+            putIn({fieldName: "reimbursementAttribute", value: rule.reimbursementAttribute}, result);
+        }
+        structure.conditions.forEach(function(condition) {
+            if(structure.isFieldVisible(condition)) {
+                var qualification = rule.getQualificationByCode(condition);
+                if(qualification) {
+                    var testValue = structure.getValue(cardScheme, condition, qualification);
+                    if(testValue) {
+                        if(testValue.fieldName === "shortName"
+                            || testValue.fieldName === "MCC"
+                            || testValue.fieldName === "bin"
+                            || testValue.fieldName === "debitCardIndicator"
+                            || testValue.fieldName === "reimbursementAttribute"
+                            || testValue.fieldName === "productCode"
+                            || testValue.fieldName === "methodOfCapture"
+                            || testValue.fieldName === "refund"
+                            || testValue.fieldName === "amount"
+                            || testValue.fieldName === "authorisationCode"
+                            || testValue.fieldName === "authorised"
+                            || testValue.fieldName === "catCode"
+                            || testValue.fieldName === "cardScheme"
+                            || testValue.fieldName === "mailOrder"
+                            || testValue.fieldName === "cardValidationRespCode"
+                            || testValue.fieldName === "isRecurring"
+                            || testValue.fieldName === "isRefund"
+                            || testValue.fieldName === "dataLevel"
+                            || testValue.fieldName === "country"
+                            || testValue.fieldName === "isChipCardRange"
+                            || testValue.fieldName === "regulatedValueFlag"
+                            || testValue.fieldName === "transactionRef"
+                            || testValue.fieldName === "traceId"
+                            || testValue.fieldName === "visaProductId"
+                            || testValue.fieldName === "commercialServiceId"
+                            || testValue.fieldName === "unitCost"
+                            || testValue.fieldName === "commodityCode"
+                            || testValue.fieldName === "productCode"
+                            || testValue.fieldName === "authVerificationValue"
+                            || testValue.fieldName === "company"
+                            || testValue.fieldName === "authCharInd"
+                            || testValue.fieldName === "region"
+                            || testValue.fieldName === "chipQualified"
+                            ) {
+                            if(testValue.fieldName === "MCC") {
+                                collectMcc(testValue.original);
+                            }
+                            putIn(testValue, result);
+                        }
+                           //console.log(JSON.stringify(testValue));
+                    } else {
+                        console.log("Err! cannot extract value for" + condition );
+                    }
+                } else {
+                 //   console.log("Err! Qualification not found: " + condition);
+                }
+            }
+        });
+        var fee = {};
+        if(structure.isFieldVisible("feePercentage")) {
+            fee.feePercentage = rule.feePercentage;
+        }
+        if(structure.isFieldVisible("flatFee")){
+            fee.flatFeeAmount = parseFloat( rule.flatFee.numeric);
+            if(rule.flatFee.currency!== undefined) {
+                fee.flatFeeCurrency = rule.flatFee.currency;
+            }
+        }
+        if(structure.isFieldVisible("minFee")) {
+            if(rule.minFee) {
+                fee.minFee = rule.minFee;
+            }
+        }
+        if(structure.isFieldVisible("maxFee")) {
+            if(rule.maxFee) {
+                fee.maxFee = rule.maxFee;
+            }
+        }
+        putIn({fieldName: "ruleId", value: rule.itemId +""+ rule.schemeId}, result);
+        putIn({fieldName: "fee", value: fee}, result);
+        if(merchantCountry !== undefined) {
+            if(result.merchant.country !== undefined && result.merchant.country === merchantCountry) {
+                console.log(JSON.stringify(result) +",");
+            }
+        } else {
+            console.log(JSON.stringify(result) + ",");
+        }
+
+    //console.log("---------------------------------------------------------------------------------");
+    });
+    console.log("------------------extracted MCC combinations");
+    for(var i in mccCombinations) {
+        console.log(mccCombinations[i]);
+    }
+
+
+    //console.log(JSON.stringify(structure));
+}, "test");
---
+++ testing.js
@@ -0,0 +1,256 @@
+function getNegation(data, value) {
+    var found = false;
+    for(i in data) {
+        found = false;
+        for(j in value) {
+            if(data[i] == value[j]) {
+                found = true;
+                break;
+
+            }
+        }
+        if(!found) {
+            return data[i];
+        }
+    }
+}
+
+var columns = {
+    Visa: {
+        "reimbursementAttribute":{
+            getValue: function(value) {
+                return value;
+            }
+        },
+        "C1061": {
+            getValue: function(qualification) {
+                return {fieldName: "shortName", value: "_" + qualification.value};
+            }
+        },
+        "AB": {
+            getValue: function(qualification) {
+                if(qualification.data && qualification.data.length) {
+                    return {fieldName: "bin", value: qualification.data[0], destination: "acquirer"};
+                }
+            }
+        },
+        "AC": {
+            getValue: function(qualification) {
+                if(qualification.data) {
+                    return {fieldName: "authCharInd", value: qualification.data[0], destination:"authorisation"};
+                }
+            }
+        },
+        "MC": {
+            getValue: function(qualification) {
+                return {fieldName: "MCC", value: qualification.original[0].low, destination: "merchant", original: qualification.original};
+            }
+        },
+        "C1064": {
+            getValue: function(qualification) {
+                return {fieldName: "shortName", value: "_" + qualification.value};
+            }
+        },
+        "C2002": {
+            getValue: function(qualification) {
+                return {fieldName: "bin", value: qualification.value, destination:"issuer"};
+            }
+        },
+        "DC": {
+            getValue: function(qualification) {
+                return {fieldName: "debitCardIndicator", value: qualification.value, destination: "issuer" };
+            }
+        },
+        "PR": {
+            getValue: function(qualification) {
+                if(qualification.data && qualification.data.length >0) {
+                    return {fieldName: "productCode", value: qualification.data[0], destination: "issuer" };
+                }
+            }
+        },
+        "CP": {
+            getValue: function(qualification) {
+                if(qualification.data && qualification.data.length >0 ) {
+                    var negated = false;
+                    if(qualification.op.trim() === "not one of") {
+                        negated = true;
+                    }
+
+                    return {fieldName: "methodOfCapture", value: (negated)?getNegation(["EG", "EH", "EK", "ST", "CH", "PI", "CL"], qualification.data):qualification.data[0]};
+                }
+            }
+        },
+        "DB": {
+          getValue: function(qualification) {
+             return {fieldName: "refund", value: qualification.value };
+          }
+        },
+        "TK": {
+            getValue: function(qualification) {
+                if(qualification.payload) {
+                    return {fieldName: "amount", value: parseFloat(qualification.payload.amount)};
+                }
+
+            }
+        },
+        "DD": {//Not yet required in the test as is stubbed, to be implemented later
+            getValue: function(value) {
+                return value;
+            }
+        },
+        "AR": {
+            getValue: function(qualification) {
+                if(qualification.data && qualification.data.length >0) {
+                    return {fieldName: "authorisationCode", value: qualification.data[0], destination:"authorisation"};
+                }
+            }
+        },
+        "AU": {
+            getValue: function(qualification) {
+                return {fieldName: "authorised", value: qualification.value, destination: "authorisation" };
+            }
+        },
+        "CA": {
+            getValue: function(qualification) {
+                return {fieldName: "catCode", destination:"merchant", value: qualification.data};
+            }
+        },
+        "CS": {
+            getValue: function(qualification) {
+                if(qualification.data && qualification.data.length > 0) {
+                    return {fieldName: "cardScheme", destination: "issuer", value: qualification.data[0] }
+                }
+            }
+        },
+        "MO": {
+            getValue: function(qualification) {
+                return {fieldName: "mailOrder", value: qualification.value};
+            }
+        },
+        "CV": {
+            getValue: function(qualification) {
+                if(qualification.data && qualification.data.length > 0) {
+                return {fieldName:"cardValidationRespCode", destination: "issuer", value: qualification.data[0]};
+                }
+
+            }
+        },
+        "RT": {
+            getValue: function(qualification) {
+                return {fieldName: "isRecurring", value: qualification.value, destination: "merchant"};
+            }
+        },
+        "C2092": {
+            getValue: function(qualification) {
+                return {fieldName: "isRefund", value:( qualification.value === "R"?true:false)}
+            }
+        },
+        "LV" : {
+            getValue: function(qualification) {
+                return {fieldName: "dataLevel", value: qualification.param};
+            }
+        },
+        "CT": {
+            getValue: function(qualification){
+                return {fieldName:"country", destination:"merchant", value: qualification.country};
+            }
+        },
+        "C2041" : {
+            getValue: function(qualification) {
+                return {fieldName: "company", value: qualification.value, destination: "merchant"};
+            }
+        },
+        "C2310" : {
+            getValue: function(qualification) {
+                var v = qualification.operator === 'Y'? true:false;
+                return {fieldName: "isChipCardRange", value:v, destination: "issuer" };
+            }
+        },
+        "C2201" : {
+            getValue: function(qualification) {
+                return {fieldName: "regulatedValueFlag", value : true };
+            }
+        },
+        "C2072" : {
+            getValue: function(qualification) {
+                return {fieldName: "transactionRef", value: qualification.value };
+            }
+        },
+        "C2311" : {
+            getValue: function(qualification) {
+                return {fieldName: "traceId", value:"TRACE_ID"}
+            }
+        },
+        "C2202": {
+            getValue: function(qualification) {
+                return {fieldName: "visaProductId", value: qualification.value};
+            }
+        },
+        "C2204": {
+            getValue: function(qualification) {
+                return {fieldName: "commercialServiceId", value: qualification.value, destination: "issuer"};
+            }
+        },
+        "LI028": {
+            getValue: function(qualification) {
+                return {fieldName: "unitCost", value: 10.0, destination: "invoice"};
+            }
+        },
+        "LI047": {
+            getValue: function(qualification) {
+                return {fieldName: "productCode", value: "ANY", destination: "invoice"};
+            }
+        },
+        "LI105" : {
+            getValue: function(qualification) {
+               return {fieldName: "commodityCode", value: "ANY", destination: "invoice"};
+            }
+        },
+        "AV": {
+            getValue: function(qualification) {
+                var value = qualification.value;
+                if(qualification.op.trim() === "not") {
+                    value = "X";
+                }
+                return {fieldName: "authVerificationValue", value: value};
+            }
+        },
+        "RE" : {
+            getValue: function(qualification) {
+                var destination = "merchant"
+                if(qualification.payload) {
+                    if (qualification.payload.member === "I") {
+                        destination = "issuer";
+                    } else if(qualification.payload.member === "A") {
+                        destination = "acquirer";
+                    }
+                    return {fieldName: "region", destination: destination, value: qualification.payload.original[0].value};
+                }
+            }
+        },
+        "CQ": {
+            getValue: function(qualification) {
+                return {fieldName: "chipQualified", destination: "issuer", value: qualification.value};
+            }
+        }/*,
+        "CC" : {
+            getValue: function(qualification) {
+                return {fieldName:"mm"}
+            }
+        }*/
+
+    },
+    MasterCard: {
+
+    }
+}
+
+var getValue = function (scheme, fieldName, value){
+    var builder= columns[scheme][fieldName];
+    if(builder) {
+        return builder.getValue(value);
+    }
+}
+module.exports = {
+    getValue: getValue
+}
